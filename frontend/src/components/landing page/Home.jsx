e8c99eaffb2793e5a1f6dd7e277f7bf25a9bef22
inilaSha <inilashademo@gmail.com>
2024-08-12 15:36:27 +0530

    Animate transition tried

--- frontend/src/components/landing page/Home.jsx
+++ frontend/src/components/landing page/Home.jsx
@@ -2,26 +2,29 @@ import React, { useState, useEffect } from 'react';
 import ImageCarousel from './Carousel';
 import Welcome from './Welcome';
 import './Home.css';
-import Navbar from './Navbar'
+import Navbar from './Navbar';
 
 const Home = () => {
-  const [darkMode, setDarkMode] = useState(() => localStorage.getItem('theme'))
+  const [darkMode, setDarkMode] = useState(() => localStorage.getItem('theme') === 'dark');
+  
   useEffect(() => {
-    // Apply the theme based on the state
     document.body.classList.toggle('dark-mode', darkMode);
-    // Store the theme preference in localStorage
     localStorage.setItem('theme', darkMode ? 'dark' : 'light');
   }, [darkMode]);
 
   const toggleDarkMode = () => {
     setDarkMode(prevDarkMode => !prevDarkMode);
   };
+
   return (
     <div className={`home ${darkMode ? 'dark-mode' : ''}`}>
       <Navbar toggleDarkMode={toggleDarkMode} />
-      <ImageCarousel />
-      <Welcome />
+      <div className="content-wrapper">
+        <ImageCarousel />
+        <Welcome />
+      </div>
     </div>
+
   );
 };
 
