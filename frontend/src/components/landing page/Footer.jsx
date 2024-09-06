4709145af516bfad1339711f378966746846ab57
inilaSha <inilashademo@gmail.com>
2024-09-06 10:08:48 +0530

    mini adj before correcting issues

--- frontend/src/components/landing page/Footer.jsx
+++ frontend/src/components/landing page/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <p>©All rights reserved. Made with  <span class="heart">❤</span> from Jaffna</p>
+        <p>©All rights reserved. Made with  <span class="heart">❤</span> from EduTech</p>
       </div>
     </footer>
   );
