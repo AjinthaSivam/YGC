4e63906156d109da6b5c112ee6a8836883400c4d
Valethanchan Krishnakishore <101230494+Krishnakishore25@users.noreply.github.com>
2024-10-04 22:10:50 +0530

    Final

--- backend/pastpaper/apps.py
+++ backend/pastpaper/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class PastpaperConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "pastpaper"
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Pastpaper'
